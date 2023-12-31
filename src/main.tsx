3ac609ebd668647435cf06c516a9d4bd1ddd8d52
mrochny <tpalinski@icloud.com>
2023-12-31 23:15:36 +0100

    Remove react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,13 @@ import './index.css'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import TextPlugin from 'gsap/TextPlugin'
 import gsap from 'gsap'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Root } from './screens/welcome/Root'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root/>
-  }
-])
 
 
 gsap.registerPlugin(ScrollTrigger)
 gsap.registerPlugin(TextPlugin)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}/>
+  <Root/>
 )
